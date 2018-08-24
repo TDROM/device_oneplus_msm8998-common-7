325752c69e8ffb5294e4f480cf47d5ccee368ceb
Brandon McAnsh <brandon.mcansh@gmail.com>
2018-08-24 16:29:08 -0400

    msm8998: Use vndk-27 android.hardware.gnss@1.0.so
    Change-Id: I58d5da8ef71d388ad16b58d90ddc29b8b555be02
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- common.mk
+++ common.mk
@@ -397,6 +397,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.hardware.gnss@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/android.hardware.gnss@1.0-v27.so
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
--- extract-files.sh
+++ extract-files.sh
@@ -66,4 +66,9 @@ if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
     extract "$MY_DIR"/../$DEVICE/proprietary-files.txt "$SRC" "$SECTION"
 fi
 
+BLOB_ROOT="$CANDY_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+
+patchelf --replace-needed android.hardware.gnss@1.0.so android.hardware.gnss@1.0-v27.so $BLOB_ROOT/vendor/lib64/vendor.qti.gnss@1.0_vendor.so
+patchelf --replace-needed android.hardware.gnss@1.0.so android.hardware.gnss@1.0-v27.so $BLOB_ROOT/lib64/vendor.qti.gnss@1.0.so
+
 "$MY_DIR"/setup-makefiles.sh
